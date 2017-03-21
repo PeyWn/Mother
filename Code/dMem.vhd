b0dd7a25974f44609ff6a727d8639134596bbce3
axelkarlsson <dranzi1996@hotmail.com>
2017-03-21 14:54:04 +0100

    pMem, ALU, dMem written

---
+++ Code/dMem.vhd
@@ -0,0 +1,31 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
+
+entity dMem is
+    port(
+        --Ports to connect to CPU
+      dMem_adress : in unsigned(15 downto 0);               
+      dMem_operation : in std_logic; --1 is write
+      dMem_in : in unsigned(15 downto 0);
+      dMem_out : out unsigned(15 downto 0);
+      clk : in std_logic);
+end dMem;
+
+architecture Behavioral of dMem is
+    type dMemData is array (0 to 32767) of unsigned(15 downto 0);
+
+    signal memory : dMemData;
+    
+begin
+--Write
+  process(clk) begin
+     if rising_edge(clk) and dMem_operation = '1' then
+       memory(to_integer(dMem_adress)) <= dMem_in;
+     end if;
+  end process;
+
+  --Read
+  dMem_out <= memory(to_integer(dMem_adress));
+
+end Behavioral;
--- Code/pMem.vhd
+++ Code/pMem.vhd
@@ -5,34 +5,15 @@ use IEEE.NUMERIC_STD.all;
 -- pMem interface
 entity pMem is
   port(
-    pAddr : in unsigned(15 downto 0);
-    pData : out unsigned(15 downto 0));
+    pAddr : in unsigned(9 downto 0);
+    pData : out unsigned(31 downto 0));
 end pMem;
 
 architecture Behavioral of pMem is
 
 -- program Memory
-type p_mem_t is array (0 to 15) of unsigned(15 downto 0);
-constant p_mem_c : p_mem_t :=
-  (x"0042",
-   x"00A0",
-   x"70FF",
-   x"1337",
-   x"0000",
-   x"0000",
-   x"0000",
-   x"0000",
-   x"0000",
-   x"0000",
-   x"0000",
-   x"0000",
-   x"0000",
-   x"0000",
-   x"0000",
-   x"0000");
-
-  signal p_mem : p_mem_t := p_mem_c;
-
+type p_mem_t is array (0 to 1023) of unsigned(31 downto 0);
+  signal p_mem : p_mem_t;
 
 begin  -- pMem
   pData <= p_mem(to_integer(pAddr));
