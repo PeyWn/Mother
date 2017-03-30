b68048150bf443ff8fed4a8f9da68a4fd0782799
joelnir <joel.oscarsson@hotmail.com>
2017-03-30 12:36:01 +0200

    Added template for assembler.

---
+++ Assembler/assemble.py
@@ -0,0 +1,40 @@
+"""
+Assembler for motherlode project
+
+program.mtr -> pMem.vhd
+
+Should work according to the following format
+- Add neccesary text to top of file
+- Decode instructions one at a time
+    - Read one line
+    - Split line at spaces
+    - Lookup instructions
+    - If symbolic adress
+        - Add to list of symbolic adresses
+    - If operation
+        - Write hex code of Operation
+        - Follow structure for Operation
+            - Convert registers to hex
+            - Convert constant to hex
+            - Lookup symbolic adress and calculate relative jmp
+- Add neccesary text after
+
+"""
+
+def decode_instruction(instr):
+    return 0;
+
+def decode_reg(reg):
+    return 0;
+
+def decode_const(const):
+    return 0;
+
+def dec_to_hex(decimal):
+    return 0;
+
+def bin_to_hex(binary):
+    return 0;
+
+def get_jmp(cur_line, sym_address):
+    return 0;
